d55fb744b903902b9bc8a0f4c8ea1778dfc90dc4
rasantoe <rasantos.dwfc5@ucamp.utel.edu.mx>
2022-03-20 11:20:04 -0400

    commit 1

---
+++ tarea-ay.js
@@ -0,0 +1,17 @@
+//let num_imp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+//for (let i = 0; i <= num_imp.length; i++) {
+    
+ // document.write(num_imp[i])
+  //i++
+    
+//}
+
+let numero2 = [];
+let numero = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+numero.forEach(function (dos){
+ numero2.push(dos * 5);
+ 
+});
+(numero2)
+
+document.write(numero2);
